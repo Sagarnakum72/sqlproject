3f587b2a0f043dcf3020202f07bd663d5edaf0e4
admin <admin@DESKTOP-K2I0ALO>
2024-11-13 14:21:29 +0530

    Add project files.

---
+++ dbo/Tables/jobfair.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[jobfair] (
+    [jobfairid]      INT          IDENTITY (1, 1) NOT NULL,
+    [studentname]    VARCHAR (40) NULL,
+    [studentaddress] VARCHAR (50) NULL,
+    [age]            INT          NULL,
+    PRIMARY KEY CLUSTERED ([jobfairid] ASC)
+);
+
